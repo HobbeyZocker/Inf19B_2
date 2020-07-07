8d26880ca97b02f72d2fc0f892d9122721f48499
AntonFelixReuterAwes <AntonFelixReuterAwes@10.6.122.6>
2020-07-07 14:51:07 +0200

    can read csv now

--- inf19b_2/src/inf19b_2/IO_manager.java
+++ inf19b_2/src/inf19b_2/IO_manager.java
@@ -1,12 +1,32 @@
 package inf19b_2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class IO_manager {
 
-	
-	/*
-	 * csv- datei einlesen
-	 * 
-	 * 
-	 */
-	
+	public static void readCSV(String csvPath) {
+
+		String csvFile = "C:\\Users\\AntonFelixReuterAwes\\git\\Inf19B_2\\Leistungsnachweis.csv";
+		if (csvPath != null) csvFile = csvPath;
+		String line = "";
+		String cvsSplitBy = ";";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+			while ((line = br.readLine()) != null) {
+
+				// use comma as separator
+				String[] good = line.split(cvsSplitBy);
+
+				System.out.println("Good[Auftrag= "+good[0]+", Art= " + good[1] + " , Produkt=" + good[2] + ", Atb1= " + good[3] +", Atb2= " + good[4] +", Belohnung= " + good[5] +"]");
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
--- inf19b_2/src/inf19b_2/Run.java
+++
@@ -1,5 +0,0 @@
-package inf19b_2;
-
-public class Run {
-
-}
