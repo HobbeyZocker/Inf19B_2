dfd5b94d782020fbc65411f4639dfb22e7af751b
Anton Reuter Awes <tonnbonn.ar@gmail.com>
2020-07-14 19:00:38 +0200

    Hover method added

--- inf19b_2/src/inf19b_2/UI/Controller.java
+++ inf19b_2/src/inf19b_2/UI/Controller.java
@@ -1,5 +1,6 @@
 package inf19b_2.UI;
 
+import java.util.concurrent.TimeUnit;
 import inf19b_2.UI.textures.ImageLocation;
 import javafx.scene.image.*;
 import javafx.scene.control.Button;
@@ -9,23 +10,60 @@ public class Controller {
 
 	@FXML
 	private ImageView grid_one_frame;
+	@FXML
 	private ImageView grid_two_frame;
+	@FXML
 	private ImageView grid_three_frame;
+	@FXML
 	private ImageView grid_four_frame;
+	@FXML
 	private ImageView grid_five_frame;
+	@FXML
 	private ImageView grid_six_frame;
+	@FXML
 	private ImageView grid_seven_frame;
+	@FXML
 	private ImageView grid_eight_frame;
+	@FXML
 	private ImageView grid_nine_frame;
-
+//
+	@FXML
 	private Button btn_bilanz;
+	@FXML
 	private Button btn_accept;
+	@FXML
 	private Button btn_reject;
+	@FXML
 	private Button btw_next_cont;
+	@FXML
 	private Button btw_move_cont;
+	@FXML
 	private Button btw_move_obj;
+	@FXML
 	private Button btw_destroy_obj;
+//
+	@FXML
+	private ImageView grid_one_frame_hover;
+	@FXML
+	private ImageView grid_two_frame_hover;
+	@FXML
+	private ImageView grid_three_frame_hover;
+	@FXML
+	private ImageView grid_four_frame_hover;
+	@FXML
+	private ImageView grid_five_frame_hover;
+	@FXML
+	private ImageView grid_six_frame_hover;
+	@FXML
+	private ImageView grid_seven_frame_hover;
+	@FXML
+	private ImageView grid_eight_frame_hover;
+	@FXML
+	private ImageView grid_nine_frame_hover;
 
+	// ######## Declaration done ##############
+
+	// ######## clicked on grid ##############
 	public void gridOneClicked() {
 		System.out.println("test1");
 		grid_one_frame.setImage(new Image(ImageLocation.class.getResource("ramen_grün.png").toExternalForm()));
@@ -64,7 +102,83 @@ public class Controller {
 		System.out.println("test9");
 	}
 
-	public void setStackFrameGreen(boolean[] stackNum) {
+	// ######## Hover over grid ##############
+
+	public void oneHoverOn() {
+		grid_one_frame_hover.setOpacity(1);
+	}
+
+	public void oneHoverOff() {
+		grid_one_frame_hover.setOpacity(0);
+	}
+
+	public void twoHoverOn() {
+		grid_two_frame_hover.setOpacity(1);
+	}
+	
+	public void twoHoverOff() {
+		grid_two_frame_hover.setOpacity(0);
+	}
+	
+	public void threeHoverOn() {
+		grid_three_frame_hover.setOpacity(1);
+	}
+
+	public void threeHoverOff() {
+		grid_three_frame_hover.setOpacity(0);
+	}
+
+	public void fourHoverOn() {
+		grid_four_frame_hover.setOpacity(1);
+	}
+	
+	public void fourHoverOff() {
+		grid_four_frame_hover.setOpacity(0);
+	}
+	
+	public void fiveHoverOn() {
+		grid_five_frame_hover.setOpacity(1);
+	}
+	
+	public void fiveHoverOff() {
+		grid_five_frame_hover.setOpacity(0);
+	}
+	
+	public void sixHoverOn() {
+		grid_six_frame_hover.setOpacity(1);
+	}
+	
+	public void sixHoverOff() {
+		grid_six_frame_hover.setOpacity(0);
+	}
+	
+	public void sevenHoverOn() {
+		grid_seven_frame_hover.setOpacity(1);
+	}
+	
+	public void sevenHoverOff() {
+		grid_seven_frame_hover.setOpacity(0);
+	}
+	
+	public void eightHoverOn() {
+		grid_eight_frame_hover.setOpacity(1);
+	}
+	
+	public void eightHoverOff() {
+		grid_eight_frame_hover.setOpacity(0);
+	}
+	
+	public void nineHoverOn() {
+		grid_nine_frame_hover.setOpacity(1);
+	}
+	
+	public void nineHoverOff() {
+		grid_nine_frame_hover.setOpacity(0);
+	}
+	
+	// ######## Change multiple grid color ##############
+	
+	public void setSomeFramesGreen(boolean[] stackNum) {
 		if (stackNum[0] == true)
 			grid_one_frame.setImage(new Image(ImageLocation.class.getResource("ramen_grün.png").toExternalForm()));
 		if (stackNum[1] == true)
@@ -85,7 +199,27 @@ public class Controller {
 			grid_nine_frame.setImage(new Image(ImageLocation.class.getResource("ramen_grün.png").toExternalForm()));
 	}
 
-	@FXML
+	public void setSomeFramesRed(boolean[] stackNum) {
+		if (stackNum[0] == true)
+			grid_one_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[1] == true)
+			grid_two_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[2] == true)
+			grid_three_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[3] == true)
+			grid_four_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[4] == true)
+			grid_five_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[5] == true)
+			grid_six_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[6] == true)
+			grid_seven_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[7] == true)
+			grid_eight_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+		if (stackNum[8] == true)
+			grid_nine_frame.setImage(new Image(ImageLocation.class.getResource("ramen_red.png").toExternalForm()));
+	}
+
 	public void setAllBlack() {
 
 		grid_one_frame.setImage(new Image(ImageLocation.class.getResource("ramen_schwarz.png").toExternalForm()));
